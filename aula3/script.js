7024e5e87eac66747b1573044b90a4d92527e1a8
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-21 20:53:18 -0300

    aprendendo a utilizar o if else

---
+++ aula3/script.js
@@ -0,0 +1,21 @@
+let idade = 30;
+let nome = 'José';
+let sexo = 'M';
+
+if (idade >= 18){
+    console.log(`Você é de maior`);
+}
+else{
+    console.log(`você é de menor`);
+}
+
+if (nome == 'Maria'){
+    console.log(`Bem vinda, maria`);
+} else{
+    console.log(`Você não é Maria`);
+}
+if (sexo == 'M'){
+    console.log(`Seu sexo é Masculino`);
+} else{
+    console.log(`Seu sexo é Feminino`);
+}
