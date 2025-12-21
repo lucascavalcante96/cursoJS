834e096aa93350054eaa15197841498842bd92c0
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-21 20:38:59 -0300

    template strings

--- aula2/script.js
+++ aula2/script.js
@@ -9,7 +9,7 @@ let divisao = valor1 / valor2;
 
 let multiplicacao = valor1 * valor2;
 
-console.log('A soma de', valor1, '+', valor2, 'é de: ', soma);
-console.log('A subtração de,', valor1, '-', valor2, 'é de ', subtracao);
-console.log('A divisão entre os valores,', valor1, 'e', valor2,'é de ', divisao);
-console.log('A multiplicação de,', valor1, 'X', valor2,'é de ', multiplicacao);
+console.log(`A soma de ${valor1}+ ${valor2} é de: ${soma}`);
+console.log(`A subtração de ${valor1} - ${valor2} é de ${subtracao}`);
+console.log(`A divisão entre os valores, %{valor1} e ${valor2} é de ${divisao}`);
+console.log(`A multiplicação de ${valor1} X ${valor2} é de ${multiplicacao}`);
