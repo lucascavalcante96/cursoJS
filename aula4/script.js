8a18e22921eb7b8501184233dc2fc34bb43a3705
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-21 21:11:27 -0300

    condicionais %%(and) e || (or)

---
+++ aula4/script.js
@@ -0,0 +1,14 @@
+let idade = 30;
+
+if (idade >= 18 && idade <= 60){
+    console.log(`Você é adulto`);
+}
+else{
+    console.log(`você Não é um adulto`);
+}
+
+let estado = "SP";
+
+if (estado == 'SP' || estado == 'RJ' || estado == 'MG' || estado == 'ES'){
+    console.log(`É um estado do Suldeste`)
+}
