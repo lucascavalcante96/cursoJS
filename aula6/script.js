b189cbd19f5c67bf8fbf7adf48d9bede8014de7d
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-26 18:14:51 -0300

    aprendendo a utilizar o return

--- aula6/script.js
+++ aula6/script.js
@@ -5,9 +5,10 @@ gravidade()
 
 function somar(valor1, valor2) {
     let soma = valor1 + valor2;
-    console.log(`O valor da soma entre ${valor1} e ${valor2} é de ${soma}`)
+    return soma;
 }
 let n1 = 10;
 let n2 = 15;
 
-somar(n1, n2);
+let resultado = somar(n1, n2);
+console.log(resultado);
