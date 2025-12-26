2197b9ddc9999195775bba2b9ae6c1209e7412ec
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-26 18:08:39 -0300

    utilizando variaveis na função

--- aula6/script.js
+++ aula6/script.js
@@ -2,3 +2,12 @@ function gravidade() {
     console.log("A gravidade do planeta é de: 9.8");
 }
 gravidade()
+
+function somar(valor1, valor2) {
+    let soma = valor1 + valor2;
+    console.log(`O valor da soma entre ${valor1} e ${valor2} é de ${soma}`)
+}
+let n1 = 10;
+let n2 = 15;
+
+somar(n1, n2);
