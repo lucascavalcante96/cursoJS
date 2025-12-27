689556aacb7567634516b9a832913cf2bc79a1f3
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-27 20:19:32 -0300

    juntar todos os exercicios anteriores e ao final verificar o tamanho do array

---
+++ exercicios/arrays/ex04.js
@@ -0,0 +1,19 @@
+/*juntar todos os exercicios anteriores e ao final verificar o tamanho do array */
+
+let carros = ['bmw', 'ferrari', 'mercedes'];
+
+/*qual o index que pega "ferrari"*/
+
+console.log(carros[1]);
+
+/*troque "Ferrari" por "Audi" */
+
+carros[1] = 'audi';
+
+console.log(`Foi trocado e agora o array tem 'audi array = [${carros}]`);
+
+/*Adicionar "Volvo" a lista */
+
+carros.push('volvo');
+
+console.log(`Array final com Volvo e Audi como resultado final array = [${carros}]`);
