f6d729ed49e4bacca66c95ede3c4ffe9195c4f2b
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-27 20:40:18 -0300

    aprendendo a utilizar e acessar os objetos

---
+++ aula11/script.js
@@ -0,0 +1,13 @@
+let dados = {
+    'nome': 'Lucas',
+    'sobrenome' : 'Cavalcante',
+    'idade' : 30,
+    'cidade': 'francisco morato',
+    'estado' : 'São paulo'
+}
+
+console.log(dados);
+/* Acessando os atributos de um objeto */
+
+console.log(`Seu nome é ${dados.nome}`);
+console.log(`Seu sobrenome é ${dados.sobrenome}`);
