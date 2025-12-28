75c7bea6b98cf8dd06bd2720f6cf5216de9ea37f
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-27 23:00:05 -0300

    algumas funções do array

---
+++ aula18/script.js
@@ -0,0 +1,26 @@
+let fruits = ['maça', 'banana', 'uva', 'morango'];
+
+/*Funções de um array */
+
+/*Adicionando um item novo ao final*/
+
+fruits.push('kiwi');
+
+//adicionando um item novo no inicio
+
+fruits.unshift('abacaxi');
+
+//removendo itens do array (ultimo)
+
+fruits.pop();
+
+//removendo o primeiro item do array
+
+fruits.shift();
+
+//retornando o array em uma string
+
+console.log(fruits.join('-'));
+
+/* Verificando o tamanho do array */
+console.log(fruits.length)
