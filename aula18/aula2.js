7b0a608f0540a7dfd76b16d4d9a44c8e988b18af
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2025-12-27 23:13:03 -0300

    utilizando o sort e o reverse

---
+++ aula18/aula2.js
@@ -0,0 +1,19 @@
+let fruits = ['banana', 'uva', 'morango', 'melancia'];
+
+//ordenando em ordem crescente
+fruits.sort();
+
+console.log(fruits);
+
+//invertendo a ordem do array
+
+let fruits2 = ['banana', 'uva', 'morango', 'melancia'];
+fruits2.reverse();
+
+console.log(fruits2);
+
+//caso queira colocar na ordem decrescente usar o sort e depois o reverse
+
+fruits.reverse();
+
+console.log(fruits);
