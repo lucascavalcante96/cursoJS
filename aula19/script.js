1adcef69ac74b4be742627fd0fa59fb42f7cdfac
lucascavalcante96 <159588768+lucascavalcante96@users.noreply.github.com>
2026-01-04 20:14:49 -0300

    aprendendo a manipular o DOM

---
+++ aula19/script.js
@@ -0,0 +1,14 @@
+/* aprendendo a manipular o DOM */
+
+document.getElementsByTagName("h1");
+
+/* no comando acima peguei todos os elementos que tem a tag h1 */
+
+document.getElementById('teste');
+
+/* no comando acima peguei todos os elementos que tem a o id teste */
+
+
+document.getElementsByClassName('container');
+
+/* no comando acima peguei todos os elementos que tem a class container */
